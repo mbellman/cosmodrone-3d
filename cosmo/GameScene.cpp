63d387fdd4ab590282f4307d09dfab2b8564ebf5
unknown <mbellman@nwe.com>
2022-02-17 13:01:30 -0600

    Preliminary third-person camera behavior

--- cosmo/GameScene.cpp
+++ cosmo/GameScene.cpp
@@ -4,32 +4,45 @@
 void GameScene::init() {
   using namespace Gamma;
 
-  Gm_EnableFlags(Gamma::FREE_CAMERA_MODE);
+  addCubeLattice();
 
-  addMesh("cube", 1000, Mesh::Cube());
+  auto& sun = createLight(LightType::DIRECTIONAL);
 
-  for (int x = 0; x < 10; x++) {
-    for (int y = 0; y < 10; y++) {
-      for (int z = 0; z < 10; z++) {
-        auto& cube = createObjectFrom("cube");
+  sun.color = Vec3f(1.0f, 0.8f, 0.2f);
+  sun.direction = Vec3f(0.5f, -1.0f, 1.0f);
 
-        cube.scale = 10.0f;
+  addMesh("drone", 1, Mesh::Cube());
 
-        cube.position = Vec3f(
-          (x - 5) * 200.0f,
-          (y - 5) * 200.0f,
-          (z - 5) * 200.0f
-        );
+  auto& drone = createObjectFrom("drone");
 
-        commit(cube);
-      }
-    }
-  }
+  drone.scale = Vec3f(10.0f, 10.0f, 30.0f);
+  drone.color = pVec4(200, 0, 200);
+  drone.position = Vec3f(50.0f, 0, 0);
 
-  input.on<MouseMoveEvent>("mousemove", [&](const MouseMoveEvent& event) {
+  commit(drone);
+
+  // @todo bring into core
+  auto updateCamera = [&]() {
+    Vec3f pos = Vec3f(
+      cosf(t_camera.altitude) * cosf(t_camera.azimuth) * t_camera.radius,
+      sinf(t_camera.altitude) * t_camera.radius,
+      cosf(t_camera.altitude) * sinf(t_camera.azimuth) * t_camera.radius
+    );
+
+    camera.position = drone.position + pos;
+
+    lookAt(drone);
+  };
+
+  updateCamera();
+
+  input.on<MouseMoveEvent>("mousemove", [&, updateCamera](const MouseMoveEvent& event) {
     if (SDL_GetRelativeMouseMode()) {
-      camera.orientation.pitch += event.deltaY / 1000.0f;
-      camera.orientation.yaw += event.deltaX / 1000.0f;
+      // @todo bring into core
+      t_camera.altitude += event.deltaY / 1000.0f;
+      t_camera.azimuth -= event.deltaX / 1000.0f;
+
+      updateCamera();
     }
   });
 
@@ -60,4 +73,28 @@ void GameScene::destroy() {
 
 void GameScene::update(float dt) {
   // ...
+}
+
+void GameScene::addCubeLattice() {
+  using namespace Gamma;
+
+  addMesh("cube", 1000, Mesh::Cube());
+
+  for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 10; y++) {
+      for (int z = 0; z < 10; z++) {
+        auto& cube = createObjectFrom("cube");
+
+        cube.scale = 10.0f;
+
+        cube.position = Vec3f(
+          (x - 5) * 200.0f,
+          (y - 5) * 200.0f,
+          (z - 5) * 200.0f
+        );
+
+        commit(cube);
+      }
+    }
+  }
 }
--- cosmo/GameScene.h
+++ cosmo/GameScene.h
@@ -1,5 +1,13 @@
 #include "Gamma.h"
 
+// @todo bring into core; add a method on the
+// struct for computing spherical coordinates
+struct TrackingCamera {
+  float azimuth = 0.0f;
+  float altitude = 0.0f;
+  float radius = 100.0f;
+};
+
 class GameScene : public Gamma::AbstractScene {
 public:
   virtual void init() override;
@@ -7,5 +15,7 @@ public:
   virtual void update(float dt) override;
 
 private:
-  // ...
+  void addCubeLattice();
+
+  TrackingCamera t_camera;
 };
